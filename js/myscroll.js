9010c670c8c7d6543de5c2a95444edfce2d21d5d
Ange Kap <angekap@hotmail.co.uk>
2016-04-20 00:30:22 +0100

    Changed the scroll JS
    * removed the space class from the panels
    * changed the JS to add the space class to panels on click

--- js/myscroll.js
+++ js/myscroll.js
@@ -3,13 +3,15 @@ $(document).ready(function(){
 	//Scroll to Home page
 	$(function () { 
 		$("#Home").click(function () { 
-   	 		$("body").scrollTo($("#panel1"), 1200); 
+			$("#panel1").addClass("space");
+   	 		$("body").scrollTo($("#panel1"), 1200);
 		}); 
 	}); 
 
 	//Scroll to Our Services page
 	$(function () { 
 		$("#Services").click(function () { 
+			$("#panel2").addClass("space")
    	 		$("body").scrollTo($("#panel2"), 1200); 
 		}); 
 	});
@@ -17,7 +19,15 @@ $(document).ready(function(){
 	//Scroll to Contact Us page
 	$(function () { 
 		$("#Contact").click(function () { 
+			$("#panel3").addClass("space")
    	 		$("body").scrollTo($("#panel3"), 1200); 
 		}); 
-	}); 
+	});
+
+	//add space class to panel 1
+	// $(function () {
+	// 	$("#Home").click(function () {
+	// 		$("#panel1").addClass("space");
+	// 	});
+	// });
 });
